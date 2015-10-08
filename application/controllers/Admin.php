1bf807ab33cb0556dd8590b276e9de29394fbe0e
oscarSlack <oscar.m@newchannel.mx>
2015-10-08 17:28:00 -0500

    Tablas
    cuando se le da aceptar o rechazar a algún usuario, de la tabla se
    borra la linea, y se manda el correo.

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,4 +1,4 @@
-  <?php
+<?php
       /**
       * Controlador para trabajar con la informacion que puede ver los administradores,
       * del sitio, la tabla para aceptar las solicitudes que nos lleguen.
@@ -9,8 +9,10 @@
       {
           // carga del constructor
           public function index(){
+            //$this->load->library('session');
             $data['title'] = "Administradores";
-            $this->load->view('templates/header', $data);
+            $data['login_status'] = true;
+            $this->load->view('templates/headerAdmin', $data);
             $this->load->view('admin/Admin_vista');
             $this->load->view('templates/footer2');
           }
@@ -21,22 +23,26 @@
               $usuario = $this->input->post('user');
               $password = $this->input->post('password');
               if( $this->Admin_model->login($usuario, $password) != false ){
-                $ingresado = true;
-                $this->load->library('session');
-                $datosAca = array('user'=>$usuario,'ingresado'=>$ingresado);
-                $this->session->set_userdata($datosAca);
                 $data['title'] = "Control";
-                $data['administrador'] = $this->session->user;
-                $this->load->view('templates/header', $data);
+                $data['administrador'] = $usuario;
+                $data['user'] = $usuario;
+                $_SESSION['status'] = true;
+                $this->load->view('templates/headerAdmin', $data);
                 $this->load->view('admin/control', $data);
                 $this->load->view('templates/footer2');
               }else{
-                $ingresado = false;
                 $data['title'] = "Control";
-                $this->load->view('templates/header', $data);
-                $this->load->view('admin/error');
+                $_SESSION['status'] = false;
+                $data['status'] = $_SESSION['status'];
+                $this->load->view('templates/headerAdmin', $data);
+                $this->load->view('admin/error', $data);
                 $this->load->view('templates/footer2');
               }
+          }
+          //cerrar sesion
+          public function cerrar(){
+            $_SESSION['status'] = false;
+            return session_destroy();
           }
           //carga los datos a la tabla por aceptar
           public function porAceptar(){
@@ -62,4 +68,3 @@
             return $this->Admin_model->insertaCodigoGenerado($codigo);
           }
       }
-  ?>
--- application/views/admin/control.php
+++ application/views/admin/control.php
@@ -1,145 +1,148 @@
-
-<nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Encuestas Intermed</a>
-        </div>
-        <div id="navbar" class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#" id = "pAceptar">Por aceptar</a></li>
-            <li><a href="#" id = "nAceptar">No aceptados</a></li>
-            <li><a href="#" id = "paceptados">aceptados</a></li>
-            <li><a href = "#" id = "">salir</a></li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-</nav>
-
-<div id="admin-page" class="container">
-
-  <div class="starter-template">
-    <h1>Bienvenido administrador <?php echo $administrador; ?></h1>
-    <h2 id="mensaje"></h2>
-    <p class="lead">Esta es la pagina de inicio</p>
-  </div>
-  <div id = "porAceptar" class = "hidden">
-    <h1>Estas son las solicitudes que han llegado</h1>
-    <table id = "pa" class = "table table-hover table-striped table-bordered">
-        <thead>
-            <tr class = "warning">
-              <th><center>#</center></th>
-              <th><center>Nombre</center></th>
-              <th><center>Correo</center></th>
-              <th><center>Cedula</center></th>
-              <th><center>Justificacion</center></th>
-              <th><center>Aceptar</center></th>
-              <th><center>Rechazar</center></th>
-            </tr>
-        </thead>
-        <tbody id = "datosPa"></tbody>
-    </table>
-  </div>
-  <div id = "noAceptadas" class = "hidden">
-    <h1>Estas son las solicitudes que no se aceptaron</h1>
-    <table id = "nAc" class = "table table-bordered table-striped">
-        <thead>
-            <tr class = "danger">
-              <th><center>#</center></th>
-              <th><center>Nombre</center></th>
-              <th><center>Correo</center></th>
-              <th><center>Cedula</center></th>
-              <th><center>Justificacion</center></th>
-            </tr>
-        </thead>
-        <tbody id = "nAceptados"></tbody>
-    </table>
-  </div>
-  <div id = "aceptados" class = "hidden">
-    <h1>Estas son las solicitudes que estan aceptadas</h1>
-    <table id = "ace" class = "table table-bordered">
-        <thead>
-            <tr class = "success">
-              <th><center>#</center></th>
-              <th><center>Nombre</center></th>
-              <th><center>Correo</center></th>
-              <th><center>Cedula</center></th>
-              <th><center>Justificacion</center></th>
-            </tr>
-        </thead>
-        <tbody id = "idAceptados"></tbody>
-    </table>
-  </div>
-</div><!-- /.container -->
-<!-- modal -->
-<div class = "modal fade" id = "aceptarModal" tabindex = "-1" role="dialog">
-    <div class = "modal-dialog" role="document">
-      <div class = "modal-content">
-        <div class = "modal-header">
-          <button type = "button" class = "close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden = "true">&times;</span>
-          </button>
-          <h4 class = "modal-title">Enviar mensaje de aceptado</h4>
-        </div>
-        <div class = "modal-body">
-          <div class = "">
-            <div class = "row">
-                <div class = "col-md-6">
-                  <div class = "col-md-12"><label>Generar código para:&nbsp;<br /><span id="codigoCorreo"></span></label></div>
-                  <hr>
-                  <div class = "input-group">
-                    <span class = "input-group-btn">
-                      <button class = "btn btn-danger" id ="generaCodigo" type ="button"><span class = "glyphicon glyphicon-fire"></span></button>
-                    </span>
-                    <input type = "text" id="aleatorioDato"class = "form-control" readonly/>
-                  </div>
-                  <div class = "col-md-12">
-                    <button type = "button" id = "enviaCodigoGenerado" class = "form-control btn btn-primary">Enviar codigo</button>
-                  </div>
-                </div>
-                <div class = "col-md-6 separador">
-                  <div class = "col-md-12">
-                    <textarea class = "form-control" id = "mensajeAceptado"></textarea>
-                  </div>
-                  <div class = "col-md-12">
-                    <button type = "button" class = "form-control btn btn-danger" id = "enviarMensaje">Enviar mensaje</button>
-                  </div>
-                </div>
-            </div>
+<?php if( isset($_SESSION['status']) == 1 ){?>
+  <nav class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Encuestas Intermed</a>
           </div>
+          <div id="navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="#" id = "pAceptar">Por aceptar</a></li>
+              <li><a href="#" id = "nAceptar">No aceptados</a></li>
+              <li><a href="#" id = "paceptados">aceptados</a></li>
+              <li><a href = "#" id = "salir">salir</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
         </div>
-      </div>
+  </nav>
+  <div id="admin-page" class="container">
+    <div class="starter-template">
+      <h1>Bienvenido administrador <?php echo "SESSION: ".$_SESSION['status']." ".$administrador; ?></h1>
+      <h2 id="mensaje"></h2>
+      <p class="lead">Esta es la pagina de inicio</p>
     </div>
-</div>
-<!-- SEGUNDO MODAL -->
-<div class = "modal fade" id = "NoaceptarModal" tabindex = "-1" role="dialog">
-    <div class = "modal-dialog" role="document">
-      <div class = "modal-content">
-        <div class = "modal-header">
-          <button type = "button" class = "close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden = "true">&times;</span>
-          </button>
-          <h4 class = "modal-title">Rechazados</h4>
-        </div>
-        <div class = "modal-body">
-          <div class ="">
-            <div class = "row">
-              <div class = "col-md-12">
-                  <div class = "col-md-12">
-                    <textarea class = "form-control" id = "areaRechazado"></textarea>
+    <div id = "porAceptar" class = "hidden">
+      <h1>Estas son las solicitudes que han llegado</h1>
+      <table id = "pa" class = "table table-hover table-striped table-bordered">
+          <thead>
+              <tr class = "warning">
+                <th><center>#</center></th>
+                <th><center>Nombre</center></th>
+                <th><center>Correo</center></th>
+                <th><center>Cedula</center></th>
+                <th><center>Justificacion</center></th>
+                <th><center>Aceptar</center></th>
+                <th><center>Rechazar</center></th>
+              </tr>
+          </thead>
+          <tbody id = "datosPa"></tbody>
+      </table>
+    </div>
+    <div id = "noAceptadas" class = "hidden">
+      <h1>Estas son las solicitudes que no se aceptaron</h1>
+      <table id = "nAc" class = "table table-bordered table-striped">
+          <thead>
+              <tr class = "danger">
+                <th><center>#</center></th>
+                <th><center>Nombre</center></th>
+                <th><center>Correo</center></th>
+                <th><center>Cedula</center></th>
+                <th><center>Justificacion</center></th>
+              </tr>
+          </thead>
+          <tbody id = "nAceptados"></tbody>
+      </table>
+    </div>
+    <div id = "aceptados" class = "hidden">
+      <h1>Estas son las solicitudes que estan aceptadas</h1>
+      <table id = "ace" class = "table table-bordered">
+          <thead>
+              <tr class = "success">
+                <th><center>#</center></th>
+                <th><center>Nombre</center></th>
+                <th><center>Correo</center></th>
+                <th><center>Cedula</center></th>
+                <th><center>Justificacion</center></th>
+              </tr>
+          </thead>
+          <tbody id = "idAceptados"></tbody>
+      </table>
+    </div>
+  </div><!-- /.container -->
+  <!-- modal -->
+  <div class = "modal fade" id = "aceptarModal" tabindex = "-1" role="dialog">
+      <div class = "modal-dialog" role="document">
+        <div class = "modal-content">
+          <div class = "modal-header">
+            <button type = "button" class = "close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden = "true">&times;</span>
+            </button>
+            <h4 class = "modal-title">Enviar mensaje de aceptado</h4>
+          </div>
+          <div class = "modal-body">
+            <div class = "">
+              <div class = "row">
+                  <div class = "col-md-6">
+                    <div class = "col-md-12"><label>Generar código para:&nbsp;<br /><span id="codigoCorreo"></span>
+                      <span id = "codigoUser" class ="hidden"></span>
+                    </label></div>
+                    <hr>
+                    <div class = "input-group">
+                      <span class = "input-group-btn">
+                        <button class = "btn btn-danger" id ="generaCodigo" type ="button"><span class = "glyphicon glyphicon-fire"></span></button>
+                      </span>
+                      <input type = "text" id="aleatorioDato"class = "form-control" readonly/>
+                    </div>
+                    <div class = "col-md-12">
+                      <button type = "button" id = "enviaCodigoGenerado" class = "form-control btn btn-primary">Enviar codigo</button>
+                    </div>
                   </div>
-                  <div class = "col-md-12">
-                    <button type = "button" class = "form-control btn btn-success"id = "envioRechazado">Enviar</button>
+                  <div class = "col-md-6 separador">
+                    <div class = "col-md-12">
+                      <textarea class = "form-control" id = "mensajeAceptado"></textarea>
+                    </div>
+                    <div class = "col-md-12">
+                      <button type = "button" class = "form-control btn btn-danger" id = "enviarMensaje">Enviar mensaje</button>
+                    </div>
                   </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-</div>
+  </div>
+  <!-- SEGUNDO MODAL -->
+  <div class = "modal fade" id = "NoaceptarModal" tabindex = "-1" role="dialog">
+      <div class = "modal-dialog" role="document">
+        <div class = "modal-content">
+          <div class = "modal-header">
+            <button type = "button" class = "close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden = "true">&times;</span>
+            </button>
+            <h4 class = "modal-title">Rechazados</h4>
+            <label>Enviar correo de rechazo a: <span id="rechazos"></span>
+            <span id = "rechazosID"></span></label>
+          </div>
+          <div class = "modal-body">
+            <div class ="">
+              <div class = "row">
+                <div class = "col-md-12">
+                    <div class = "col-md-12">
+                      <textarea class = "form-control" id = "areaRechazado"></textarea>
+                    </div>
+                    <div class = "col-md-12">
+                      <button type = "button" class = "form-control btn btn-success"id = "envioRechazado">Enviar</button>
+                    </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
+<?php }?>
