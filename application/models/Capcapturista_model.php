4d8960b32f74b0624da8317a34233e1630e54cab
cinthiabNewChannel <cinthia.b@newchannel.mx>
2015-11-13 16:32:40 -0600

    restar 120 minutos por dia

--- application/models/Capcapturista_model.php
+++ application/models/Capcapturista_model.php
@@ -43,8 +43,12 @@
       $result = $this->db_capturista->get()->row_array();
       $to_time = strtotime($result['MAX']);
       $from_time = strtotime($result['MIN']);
+      $min = intval(round(abs($to_time - $from_time) / 60,2));
 
-      $result['minutos'] = intval(round(abs($to_time - $from_time) / 60,2));
+      if ($min > 120){
+        $min -= 120;
+      }
+      $result['minutos'] = $min;
       return $result;
     }
 
@@ -59,9 +63,12 @@
         $totalRegistros += intval($Query[$i]['TOTAL']);
         $to_time = strtotime($Query[$i]['MAX']);
         $from_time = strtotime($Query[$i]['MIN']);
-
-        $Query[$i]['minutos'] = intval(round(abs($to_time - $from_time) / 60,2));
-        $minutos += intval(round(abs($to_time - $from_time) / 60,2));
+        $min = intval(round(abs($to_time - $from_time) / 60,2));
+        if ($min > 120){
+          $min -= 120;
+        }
+        $Query[$i]['minutos'] = $min;
+        $minutos += $min;
       };
       $result['total'] = $totalRegistros;
       $result['minutos'] = $minutos;
