efc2834c50673a2144609d41686509d9dc9f434a
cinthiabNewChannel <cinthia.b@newchannel.mx>
2015-11-25 11:50:17 -0600

    base_url actualizado

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/encuesta-intermed/';
+$config['base_url'] = 'http://www.intermed.online/';
 /*
 |--------------------------------------------------------------------------
 | Index File
