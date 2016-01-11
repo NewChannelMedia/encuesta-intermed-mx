cc204ac461dc77c0d2cb9ae0691d7e3bdfa54432
Juan Carlos Medina <jcmedina@newchannel.mx>
2016-01-11 10:38:29 -0600

    new home

--- application/views/index.php
+++ application/views/index.php
@@ -68,38 +68,36 @@
           </div>
         </div>
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-          <div class="main-body">
-            <div class="container">
-              <div class="row">
-                <div class="main-body-container col-lg-6 col-lg-offset-3 col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1">
-                  <div class="row">
-                    <div class="col-md-12 col-sm-12 col-xs-12 code-info">
-                      <h4 class="flama-normal s20 text-center">
-                        <span class="ag-medium">Intermed<sup>®</sup></span> se encuentra en  desarrollo. <br>
-                        Si has recibido una invitación para participar en la encuesta, ingresa tu código para entrar y conocer mas acerca del proyecto y los servicios que tiene para ti.
-                      </h4>
-                    </div>
-                    <div class="col-md-12 code-info">
-                      <h4 class="flama-bold s20 text-center">Ingresa tu código aquí:</h4>
-                    </div>
-                    <div class="col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1 code-input">
-                      <form role="form" method="POST" action="<?= base_url(); ?>about">
-                        <div class="form-group">
-                          <input type="text" class="form-control code-intput-control" placeholder="" name="codigo">
-                        </div>
-                        <div class="form-group">
-                          <input type="submit" value="Enviar" class="btn btn-default center-block code-intput-button">
-                        </div>
-                      </form>
-                    </div>
-                  </div>
+
+        </div>
+      </div>
+      <div class="row">
+        <div class="main-body clearfix">
+          <div class="main-body-container col-lg-6 col-lg-offset-3 col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 col-xs-12">
+            <div class="code-info">
+              <h4 class="flama-normal s20 text-center">
+                <span class="ag-medium">Intermed<sup>®</sup></span> se encuentra en  desarrollo. <br>
+                Si has recibido una invitación para participar en la encuesta, ingresa tu código para entrar y conocer mas acerca del proyecto y los servicios que tiene para ti.
+              </h4>
+            </div>
+            <div class="code-info">
+              <h4 class="flama-bold s20 text-center">Ingresa tu código aquí:</h4>
+            </div>
+            <div class="code-input">
+              <form role="form" method="POST" action="<?= base_url(); ?>about">
+                <div class="form-group">
+                  <input type="text" class="form-control code-intput-control" placeholder="" name="codigo">
                 </div>
-              </div>
+                <div class="">
+                  <input type="submit" value="Enviar" class="btn btn-default center-block code-intput-button">
+                </div>
+              </form>
             </div>
           </div>
         </div>
       </div>
-      <div class="row">
+    </div>
+    <div class="row">
         <div class="main-extra">
           <div class="container">
             <div class="col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2 col-xs-10 col-xs-offset-1">
@@ -111,7 +109,7 @@
           </div>
         </div>
       </div>
-    </div>
+
   </div>
 </header>
 <section class="main">
